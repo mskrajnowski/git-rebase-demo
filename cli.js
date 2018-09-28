0d667e3e0617db5d76381e985d6b5a2fc31876c7
Marek Skrajnowski <marek@codequest.com>
2018-09-28 13:36:53 +0200

    Added cwd command

--- cli.js
+++ cli.js
@@ -8,6 +8,11 @@ const version = require("./package.json").version
 cli.version(version)
 
 cli
+  .command("cwd")
+  .description("Prints the current working directory")
+  .action(() => console.log(process.cwd()))
+
+cli
   .command("ls [path]")
   .description("List files in a directory")
   .option("-a, --all", "List hidden files")
