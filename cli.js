0b4e6c495c522e1ea79f9385c33b90ef81c8dbbe
Marek Skrajnowski <marek@codequest.com>
2018-09-28 13:43:48 +0200

    Display help only when an unknown command is passed

--- cli.js
+++ cli.js
@@ -3,6 +3,14 @@
 const cli = require("commander")
 const version = require("./package.json").version
 
-cli.version(version).parse(process.argv)
+cli.version(version)
 
-cli.help()
+cli.on("command:*", () => {
+  if (cli.args.length) {
+    console.error("Invalid command: %s\n", cli.args.join(" "))
+  }
+
+  cli.help()
+})
+
+cli.parse(process.argv)
